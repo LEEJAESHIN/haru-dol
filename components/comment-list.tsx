a1c86cf42359ac6e2d054f7e75fd60a6a314adcc
jslee-ncurity <jslee@ncurity.com>
2024-08-09 12:42:11 +0900

    add dialog delete

--- components/comment-list.tsx
+++ components/comment-list.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Cross2Icon } from '@radix-ui/react-icons';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon, PaperPlaneIcon } from '@radix-ui/react-icons';
 
 export default function guestbook() {
   const [users, setUsers] = useState([]);
-  
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -17,10 +19,11 @@ export default function guestbook() {
     setUsers(data);
   };
 
-  const deleteUser = async (id: number) => {
+  const deleteUser = async (id: number, userInfo: any) => {
     console.log(id)
     await fetch(`/api/users?id=${id}`, {
       method: 'DELETE',
+      body: JSON.stringify(userInfo),
     })
       .then(() => {
         fetchUsers()
@@ -35,6 +38,20 @@ export default function guestbook() {
     //   console.error('Failed to delete user');
     // }
   };
+  const hash = () => {
+
+  }
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>, id: number) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+
+    const userInfo = {
+      id,
+      password: (form.elements.namedItem('password') as HTMLInputElement).value,
+    };
+
+    deleteUser(id, userInfo)
+  }
 
   return (
     <div className='px-10 divide-y'>
@@ -45,7 +62,28 @@ export default function guestbook() {
               <span className='text-gy-6'>{item.name}</span>
               <span className='text-xxs ml-1'>2024.08.08</span>
             </div>
-            <Cross2Icon onClick={() => deleteUser(item.id)} />
+            <Dialog.Root open={open} onOpenChange={setOpen}>
+              <Dialog.Trigger asChild>
+                <Cross2Icon className='cursor-pointer'  />
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="GuestBookDlgOverlay" />
+                <Dialog.Content className="GuestBookDlgContent">
+                  <Dialog.Title className="flex justify-between font-serif mb-4">
+                    방명록 삭제
+                  </Dialog.Title>
+                  <form onSubmit={(e) => onSubmit(e, item.id)}>
+                    <label className='flex mb-2 font-serif'>
+                      <input className="Input w-full h-8 pl-2" name='password' type='password' placeholder="비밀번호" />
+                    </label>
+                    <label className='flex justify-center gap-2 mt-2 font-serif'>
+                      <button className="text-xs px-3 py-1" onClick={() => setOpen(false)}>취소</button>
+                      <button className="text-xs px-3 py-1 rounded bg-black text-white">확인</button>
+                    </label>
+                  </form>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </div>
           <div className='text-xs pr-4 mt-1'>{item.content}</div>
         </div>
